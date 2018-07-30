91ca386df0865a5f15fb22175b7ce349d18f3d0c
Courtney Yoo <c.yoo@wustl.edu>
2018-07-29 22:35:56 -0500

    Successful render of detail activity

--- app/src/main/java/com/example/android/popularmoviesstage1/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmoviesstage1/DetailActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class DetailActivity extends AppCompatActivity {
 
     private TextView mMovieTitleTextView, mRatingTextView, mReleaseDateTextView, mSynopsisTextView;
-    private ImageView mMoviePosterImageVIew;
+    private ImageView mMoviePosterImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,23 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        // Null object reference
+        mMovieTitleTextView = findViewById(R.id.movieTitle_tv);
         mMovieTitleTextView.setText(intent.getStringExtra("title"));
+
+        mRatingTextView = findViewById(R.id.rating_tv);
+        mRatingTextView.setText(String.valueOf(intent.getFloatExtra("rating", (float) 0.0)));
+
+        mReleaseDateTextView = findViewById(R.id.releaseDate_tv);
+        mReleaseDateTextView.setText(intent.getStringExtra("releaseDate"));
+
+        mSynopsisTextView = findViewById(R.id.synopsis_tv);
+        mSynopsisTextView.setText(intent.getStringExtra("synopsis"));
+
+        mMoviePosterImageView = findViewById(R.id.moviePoster_iv);
+        Picasso
+                .with(this)
+                .load("http://image.tmdb.org/t/p/" + "w185/" + intent.getStringExtra("poster"))
+                .error(R.drawable.ic_launcher_foreground)
+                .into(mMoviePosterImageView);
     }
 }
