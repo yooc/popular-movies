d1ad8554c4b8b43a7f9ed1edeb8cc802f6b1e381
Courtney Yoo <c.yoo@wustl.edu>
2018-10-09 21:57:27 -0500

    Some UI clean up

--- app/src/main/java/com/example/android/popularmoviesstage1/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmoviesstage1/DetailActivity.java
@@ -45,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
         mMoviePosterImageView = findViewById(R.id.moviePoster_iv);
         Picasso
                 .with(this)
-                .load("http://image.tmdb.org/t/p/" + "w185/" + intent.getStringExtra("poster"))
+                .load("http://image.tmdb.org/t/p/" + "w500/" + intent.getStringExtra("poster"))
                 .error(R.drawable.ic_launcher_foreground)
                 .into(mMoviePosterImageView);
 
